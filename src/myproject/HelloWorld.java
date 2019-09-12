f25f9ce197b5c750e07c139950cee46b9c180f0a
Hiroki Masuda <hirokimasuda@HirokinoMacBook-Pro.local>
2019-09-12 13:25:31 +0900

    新規作成

---
+++ src/myproject/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ *
+ */
+package myproject;
+
+/**
+ * @author Hiroki Masuda
+ *
+ */
+public class HelloWorld {
+
+    /**
+     * メインメソッド
+     * @param args
+     */
+    public static void main(String[] args) {
+        // コンソールに「Hello World!!」の文字列を表示します
+        System.out.println("Hello World!!");
+    }
+}
